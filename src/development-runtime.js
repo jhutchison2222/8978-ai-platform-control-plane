895f8ec5fa1adf3f6ffea26f09bcd090e8203987
jhutchison2222 <jhutchison@8978tech.com>
2026-08-13 13:32:53 -0600

    Compose authority readers in development runtime

--- src/development-runtime.js
+++ src/development-runtime.js
@@ -3,19 +3,34 @@ import {
   CloudflareDurableIdempotencyStore,
   CloudflareDurableOwnerDecisionStore,
 } from "./cloudflare-runtime-stores.js";
+import { createD1AuthorityRuntimeDependencies, D1_AUTHORITY_RUNTIME_DEPENDENCIES } from "./d1-authority-runtime-composition.js";
 import { REQUIRED_RUNTIME_DEPENDENCIES } from "./runtime-contracts.js";
 import { createUnavailableRuntime } from "./unavailable-runtime.js";
 
-const IMPLEMENTED = new Set(["idempotencyStore", "ownerDecisionStore", "auditStore"]);
+const DURABLE_DEPENDENCIES = Object.freeze(["idempotencyStore", "ownerDecisionStore", "auditStore"]);
 
 export const DEVELOPMENT_UNAVAILABLE_RUNTIME_DEPENDENCIES = Object.freeze(
-  REQUIRED_RUNTIME_DEPENDENCIES.filter((name) => !IMPLEMENTED.has(name)),
+  REQUIRED_RUNTIME_DEPENDENCIES.filter((name) => !DURABLE_DEPENDENCIES.includes(name)),
 );
 
+function hasAuthorityDatabase(env) {
+  return typeof env?.AUTHORITY_DB?.prepare === "function";
+}
+
+export function developmentUnavailableRuntimeDependencies(env) {
+  const implemented = new Set(DURABLE_DEPENDENCIES);
+  if (hasAuthorityDatabase(env)) for (const name of D1_AUTHORITY_RUNTIME_DEPENDENCIES) implemented.add(name);
+  return Object.freeze(REQUIRED_RUNTIME_DEPENDENCIES.filter((name) => !implemented.has(name)));
+}
+
 export function createDevelopmentRuntime(env) {
+  const authorityDependencies = hasAuthorityDatabase(env)
+    ? createD1AuthorityRuntimeDependencies(env.AUTHORITY_DB)
+    : {};
   return createUnavailableRuntime({
     idempotencyStore: new CloudflareDurableIdempotencyStore(env.IDEMPOTENCY_STORE),
     ownerDecisionStore: new CloudflareDurableOwnerDecisionStore(env.OWNER_DECISION_STORE),
     auditStore: new CloudflareDurableAuditStore(env.AUDIT_STORE),
+    ...authorityDependencies,
   });
 }
